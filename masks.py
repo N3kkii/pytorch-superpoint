5a7f09f2a014477eac9df92dccf337f42dd65482
N3kkii <vojta.ad22@gmail.com>
2025-02-20 15:16:02 +0100

    fix: code cleanup, removing images with no instances

--- masks.py
+++ masks.py
@@ -6,7 +6,14 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+from settings import EXPER_PATH, DATA_PATH
 
+input_folder = 'cars_train'
+output_folder = 'masks'
+output_path = os.path.join(EXPER_PATH, output_folder)
+
+
+os.makedirs(output_path, exist_ok = True)
 
 # Initialize the configuration
 cfg = get_cfg()
@@ -23,27 +30,24 @@ cfg.MODEL.WEIGHTS = get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # Set the threshold for predictions
 cfg.MODEL.DEVICE = "cuda"  # Use "cuda" for GPU inference or "cpu" for CPU inference
 
-output_path = "/zfs-pool/xadame44/pytorch-superpoint/segmented_image.jpg"
-image_path = "/zfs-pool/xadame44/datasets/cars_train/00001.jpg"
-image = cv2.imread(image_path)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-input_folder = '/zfs-pool/xadame44/datasets/cars_train/'
-output_folder = 'masks'
-
+output_path = os.path.join(EXPER_PATH, output_folder)
+input_path = os.path.join(DATA_PATH, input_folder)
 # Initialize the predictor
 predictor = DefaultPredictor(cfg)
 
-for file in os.listdir(input_folder):
-    img = cv2.imread(f"{input_folder}/{file}")
-    outputs = predictor(img)
+for file in os.listdir(input_path):
+    img = cv2.imread(f"{input_path}/{file}")
+    outputs = predictor(img) # Run prediction
     instances = outputs["instances"]
-    cars = instances[(instances.pred_classes == 2) | (instances.pred_classes == 7)]
+    cars = instances[(instances.pred_classes == 2) | (instances.pred_classes == 7)] # COCO classes, 2 = car, 7 = truck
+
     if len(cars) > 0:
         masks = cars.pred_masks
         areas = masks.sum(dim=[1, 2])
         largest_idx = areas.argmax()
         largest_mask = masks[largest_idx].cpu().numpy()
-        np.savez_compressed(f"{output_folder}/{file}_mask.npz", mask=largest_mask)
+        np.savez_compressed(f"{output_path}/{file}_mask.npz", mask=largest_mask)
+
     else:
         print(f"No instances of car found in {file}.")
+        os.remove(f"{input_path}/{file}")
